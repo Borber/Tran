e6309b329287989555484a2e5181e3a4a4cb930d
Borber <borber.drum@gmail.com>
2023-11-10 01:21:53 +0800

    [mod] 更新部分组件

--- src/icon.tsx
+++ src/icon.tsx
@@ -48,3 +48,51 @@ export const Close = (props: IconProps) => {
         </svg>
     )
 }
+
+export const PinIcon = (props: IconProps) => {
+    return (
+        <svg
+            data-icon="pin"
+            width={props.size}
+            height={props.size}
+            role="img"
+            viewBox="0 0 16 16">
+            <path
+                d="M9.41.92c-.51.51-.41 1.5.15 2.56L4.34 7.54C2.8 6.48 1.45 6.05.92 6.58l3.54 3.54-3.54 4.95 4.95-3.54 3.54 3.54c.53-.53.1-1.88-.96-3.42l4.06-5.22c1.06.56 2.04.66 2.55.15L9.41.92z"
+                fill-rule="evenodd"
+            />
+        </svg>
+    )
+}
+
+export const CopyIcon = (props: IconProps) => {
+    return (
+        <svg
+            data-icon="duplicate"
+            width={props.size}
+            height={props.size}
+            role="img"
+            viewBox="0 0 16 16">
+            <path
+                d="M15 0H5c-.55 0-1 .45-1 1v2h2V2h8v7h-1v2h2c.55 0 1-.45 1-1V1c0-.55-.45-1-1-1zm-4 4H1c-.55 0-1 .45-1 1v10c0 .55.45 1 1 1h10c.55 0 1-.45 1-1V5c0-.55-.45-1-1-1zm-1 10H2V6h8v8z"
+                fill-rule="evenodd"
+            />
+        </svg>
+    )
+}
+
+export const HideIcon = (props: IconProps) => {
+    return (
+        <svg
+            data-icon="cross"
+            width={props.size}
+            height={props.size}
+            role="img"
+            viewBox="0 0 16 16">
+            <path
+                d="M9.41 8l3.29-3.29c.19-.18.3-.43.3-.71a1.003 1.003 0 00-1.71-.71L8 6.59l-3.29-3.3a1.003 1.003 0 00-1.42 1.42L6.59 8 3.3 11.29c-.19.18-.3.43-.3.71a1.003 1.003 0 001.71.71L8 9.41l3.29 3.29c.18.19.43.3.71.3a1.003 1.003 0 00.71-1.71L9.41 8z"
+                fill-rule="evenodd"
+            />
+        </svg>
+    )
+}
--- src/page/Panel.tsx
+++ src/page/Panel.tsx
@@ -1,9 +1,33 @@
 import "../css/Panel.css"
 
+import { createSignal } from "solid-js"
+
+import { CopyIcon, HideIcon, PinIcon } from "../icon"
+
 const Panel = () => {
+    const [pin, Pin] = createSignal(false)
+    const [copy, Copy] = createSignal(false)
+
     return (
         <div class="panel">
-            <div data-tauri-drag-region>翻译</div>
+            <div class="panel-control">
+                <div class="panel-control-item" onClick={() => Pin(!pin())}>
+                    <span classList={{ "panel-control-pin": pin() }}>
+                        <PinIcon size={14} />
+                    </span>
+                </div>
+                <div data-tauri-drag-region class="panel-control-drag" />
+                <div
+                    class="panel-control-item"
+                    classList={{ "panel-control-copy": copy() }}
+                    onClick={() => Copy(!copy())}>
+                    <CopyIcon size={12} />
+                </div>
+                <div class="panel-control-item">
+                    <HideIcon size={14} />
+                </div>
+            </div>
+            <div class="result">Hello world!</div>
         </div>
     )
 }
