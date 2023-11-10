f1c5634587a2d938df2da9614cf5357cb2e7522a
Borber <borber.drum@gmail.com>
2023-11-10 13:20:48 +0800

    [mod] 删除关闭按钮, 更改交互逻辑, 将部分响应式变量变回普通变量

--- src/page/Panel.tsx
+++ src/page/Panel.tsx
@@ -4,29 +4,28 @@ import { invoke } from "@tauri-apps/api"
 import { getCurrent, PhysicalPosition } from "@tauri-apps/api/window"
 import { createSignal } from "solid-js"
 
-import { CopyIcon, HideIcon, PinIcon } from "../icon"
+import { CopyIcon, PinIcon } from "../icon"
 
 const Panel = () => {
     const panel = getCurrent()
 
+    let pinFlag = false
+    let moveFlag = true
+
     const [pin, Pin] = createSignal(false)
     const [copy, Copy] = createSignal(false)
-    const [hide, Hide] = createSignal(false)
-
     const [result, Result] = createSignal("")
-
-    // 移出关闭标志
-    const [moveHide, MoveHide] = createSignal(true)
 
     // 监听事件， 显示panel
     panel.listen<{ x: number; y: number }>("show", async (pos) => {
-        Copy(false)
-        Result(await invoke("translate"))
-        if (!pin()) {
+        if (!pinFlag) {
             await panel.setPosition(
                 new PhysicalPosition(pos.payload.x - 40, pos.payload.y + 20)
             )
         }
+
+        Copy(false)
+        Result(await invoke("translate"))
 
         await panel.show()
         await panel.setFocus()
@@ -36,7 +35,7 @@ const Panel = () => {
         <div
             class="panel"
             onMouseLeave={async () => {
-                if (moveHide()) {
+                if (moveFlag) {
                     await panel.hide()
                 }
             }}>
@@ -45,8 +44,9 @@ const Panel = () => {
                 <div
                     class="panel-control-item"
                     onClick={() => {
-                        MoveHide(!moveHide())
-                        Pin(!pin())
+                        moveFlag = !moveFlag
+                        pinFlag = !pinFlag
+                        Pin(pinFlag)
                     }}>
                     <span classList={{ "panel-control-pin": pin() }}>
                         <PinIcon size={14} />
@@ -58,17 +58,6 @@ const Panel = () => {
                     classList={{ "panel-control-copy": copy() }}
                     onClick={() => Copy(!copy())}>
                     <CopyIcon size={12} />
-                </div>
-                <div
-                    class="panel-control-hide"
-                    classList={{ "panel-control-hide-hover": hide() }}
-                    onMouseEnter={() => Hide(true)}
-                    onMouseLeave={() => Hide(false)}
-                    onClick={() => {
-                        Hide(false)
-                        panel.hide()
-                    }}>
-                    <HideIcon size={14} />
                 </div>
             </div>
         </div>
