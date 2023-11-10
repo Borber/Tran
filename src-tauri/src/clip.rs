3c7b9b85b15bed2e26d675ba69dfa33fc424f003
Borber <borber.drum@gmail.com>
2023-11-10 17:17:37 +0800

    [mod] 模拟复制

---
+++ src-tauri/src/clip.rs
@@ -0,0 +1,10 @@
+use arboard::Clipboard;
+
+// 获取剪贴板内容
+pub fn get() -> String {
+    let mut clipboard = Clipboard::new().unwrap();
+    match clipboard.get_text() {
+        Ok(text) => text,
+        Err(_) => "获取剪贴板内容失败".to_string(),
+    }
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,24 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use enigo::{Enigo, KeyboardControllable};
+
+mod clip;
 mod setup;
 mod shortcut;
 mod tray;
 mod window;
 
 #[tauri::command]
-fn translate() -> String {
-    "Hello, world!".to_string()
+async fn translate() -> String {
+    // TODO 发送复制按键
+    tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+    let mut enigo = Enigo::new();
+    enigo.key_sequence_parse("{+CTRL}c{-CTRL}");
+    let context = clip::get();
+    println!("context: {:?}", context);
+    context
 }
-
+//
 fn main() {
     tauri::Builder::default()
         .system_tray(tray::new())
