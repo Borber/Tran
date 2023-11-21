f80eb163c2883518d3a5afc09d9c42e77e7dbcca
Borber <borber.drum@gmail.com>
2023-11-21 09:53:51 +0800

    [mod] fix split

--- script/lang.lua
+++ script/lang.lua
@@ -37,17 +37,10 @@ print(Context)
 print("Lua script end")
 
 
-function split(input, delimiter)
-    input = tostring(input)
-    delimiter = tostring(delimiter)
-    if (delimiter == "") then return false end
-    local pos, arr = 0, {}
-    for st, sp in function() return string.find(input, delimiter, pos, true) end do
-        table.insert(arr, string.sub(input, pos, st - 1))
-        pos = sp + 1
-    end
-    table.insert(arr, string.sub(input, pos))
-    return arr
+function split(str, sep)
+    local sep, res = sep or '%s', {}
+    string.gsub(str, '[^'..sep..']+', function(x) res[#res+1] = x end)
+    return res 
 end
 
 function indexOf(array, value)
