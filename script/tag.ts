206f4ff5d5122fca8de531a22ec453d9d0aa8d1c
Borber <borber.drum@gmail.com>
2023-11-22 00:59:07 +0800

    Update

--- script/tag.ts
+++ script/tag.ts
@@ -1,7 +1,7 @@
 const dirs = ["msi", "nsis", "deb", "appimage", "dmg", "macos",]
 
 const lang = Deno.env.get("MATRIX_LANG")
-const root = "../src-tauri/target/release/bundle"
+const root = "src-tauri/target/release/bundle"
 
 for (const dir of dirs) {
     if (Deno.statSync(`${root}/${dir}`).isDirectory) {
