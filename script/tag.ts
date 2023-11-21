450e9079f527d79b80095e4c15161db15c0f66e3
Borber <borber.drum@gmail.com>
2023-11-22 01:29:51 +0800

    Update

--- script/tag.ts
+++ script/tag.ts
@@ -1,17 +1,21 @@
+import { ensureDir } from "https://deno.land/std/fs/mod.ts";
+
 const dirs = ["msi", "nsis", "deb", "appimage", "dmg", "macos",]
 
 const lang = Deno.env.get("MATRIX_LANG")
 const root = "src-tauri/target/release/bundle"
 
 for (const dir of dirs) {
-    if (Deno.statSync(`${root}/${dir}`).isDirectory) {
-        for await (const file of Deno.readDir(`${root}/${dir}`)) {
-            if (file.isFile && file.name.startsWith("tran")) {
-                let name = file.name
-                name = name.replace("tran", "tran" + "_" + lang)
-                Deno.renameSync(`${root}/${dir}/${file.name}`, `${root}/${dir}/${name}`)
-                console.log(`${root}/${dir}/${name}`)
+    ensureDir(`${root}/${dir}`).then(async () => {
+        if (Deno.statSync(`${root}/${dir}`).isDirectory) {
+            for await (const file of Deno.readDir(`${root}/${dir}`)) {
+                if (file.isFile && file.name.startsWith("tran")) {
+                    let name = file.name
+                    name = name.replace("tran", "tran" + "_" + lang)
+                    Deno.renameSync(`${root}/${dir}/${file.name}`, `${root}/${dir}/${name}`)
+                    console.log(`${root}/${dir}/${name}`)
+                }
             }
         }
-    }
+    })
 }
