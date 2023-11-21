cdaf7396bdba9ec87011f7bed5f724ad1ebc7073
Borber <borber.drum@gmail.com>
2023-11-22 00:48:30 +0800

    Update

--- script/tag.ts
+++ script/tag.ts
@@ -1,15 +1,16 @@
 const dirs = ["msi", "nsis", "deb", "appimage", "dmg", "macos",]
 
 const lang = Deno.env.get("MATRIX_LANG")
+const root = "../src-tauri/target/release/bundle"
 
 for (const dir of dirs) {
-    if (Deno.statSync(`./${dir}`).isDirectory) {
+    if (Deno.statSync(`${root}/${dir}`).isDirectory) {
         for await (const file of Deno.readDir(dir)) {
             if (file.isFile && file.name.startsWith("tran")) {
                 let name = file.name
                 name = name.replace("tran", "tran" + "_" + lang)
-                Deno.renameSync(`./${dir}/${file.name}`, `./${dir}/${name}`)
-                console.log(`./${dir}/${name}`)
+                Deno.renameSync(`${root}/${file.name}`, `${root}/${name}`)
+                console.log(`${root}/${name}`)
             }
         }
     }
