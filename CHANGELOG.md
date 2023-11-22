d5c50cb0d38363a543438750e73935798e188b28
Borber <borber.drum@gmail.com>
2023-11-22 09:31:09 +0800

    [mod] 处理多目标构建

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+# Changelog
+
+## [0.1.1]
+
+<div align="center">
+
+| **To \ Form** | **CN** |
+| :-----------: | :----: |
+|    **EN**     |   ✅   |
+|    **JP**     |   ✅   |
+
+</div>
+
+如有 Bug,请提 issue
--- script/built.ts
+++ script/built.ts
@@ -5,6 +5,8 @@ const dirs = ["msi", "nsis", "deb", "appimage", "dmg", "macos",]
 const lang = Deno.env.get("MATRIX_LANG")
 const root = "src-tauri/target/release/bundle"
 
+await Deno.mkdir("release")
+
 for (const dir of dirs) {
     ensureDir(`${root}/${dir}`).then(async () => {
         if (Deno.statSync(`${root}/${dir}`).isDirectory) {
@@ -12,8 +14,8 @@ for (const dir of dirs) {
                 if (file.isFile && file.name.startsWith("tran")) {
                     let name = file.name
                     name = name.replace("tran", "tran" + "_" + lang)
-                    Deno.renameSync(`${root}/${dir}/${file.name}`, `${root}/${dir}/${name}`)
-                    console.log(`${root}/${dir}/${name}`)
+                    await Deno.copyFile(`${root}/${dir}/${file.name}`, `release/${name}`)
+                    console.log(`release/${name}`)
                 }
             }
         }
