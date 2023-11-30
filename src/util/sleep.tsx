394e1cd02bb04b63be2fdbb20efddd15fa57f106
Borber <borber.drum@gmail.com>
2023-11-30 08:51:58 +0800

    [mod] Remove unuse code

--- src/util/sleep.tsx
+++
@@ -1,7 +0,0 @@
-export const sleep = async (ms: number) => {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(null)
-        }, ms)
-    })
-}
--- src/util/timestamp.tsx
+++
@@ -1,9 +0,0 @@
-const timestamp_number = () => {
-    return new Date().getTime()
-}
-
-const timestamp_string = () => {
-    return timestamp_number() + ""
-}
-
-export { timestamp_number, timestamp_string }
