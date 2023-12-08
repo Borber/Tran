09676a45e6267de2b5271df08af10ee43bb85848
Borber <borber.drum@gmail.com>
2023-12-08 18:41:08 +0800

    [mod] update solidjs to 0.10

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,31 @@
 import "./styles.css"
 
 import { Router } from "@solidjs/router"
+import { lazy, onMount } from "solid-js"
 import { render } from "solid-js/web"
 
-import App from "./App"
+onMount(async () => {
+    // 生产环境, 全局取消右键菜单;
+    if (!import.meta.env.DEV) {
+        document.oncontextmenu = (event) => {
+            event.preventDefault()
+        }
+    }
+})
+
+const routes = [
+    { path: "/", component: lazy(() => import("./page/Setting")) },
+    {
+        path: "/tray",
+        component: lazy(() => import("./util/tray/page/Tray")),
+    },
+    {
+        path: "/panel",
+        component: lazy(() => import("./page/Panel")),
+    },
+]
 
 render(
-    () => (
-        <Router>
-            <App />
-        </Router>
-    ),
+    () => <Router>{routes}</Router>,
     document.getElementById("root") as HTMLElement
 )
