162e29f93e88e1629caa04a2c5926f27d6bf4876
Borber <borber.drum@gmail.com>
2024-01-02 13:21:08 +0800

    Automatically display translation after selection

--- src-tauri/src/common.rs
+++ src-tauri/src/common.rs
@@ -1,7 +1,12 @@
+use std::sync::atomic::AtomicUsize;
+
 use once_cell::sync::Lazy;
 use reqwest::Client;
 
 use crate::manager;
+
+// 全局储存按下时间戳 原子类型
+pub static TIME: AtomicUsize = AtomicUsize::new(0);
 
 pub static CLIENT: Lazy<Client> = Lazy::new(Client::new);
 
--- src-tauri/src/setup.rs
+++ src-tauri/src/setup.rs
@@ -1,21 +1,52 @@
 use std::error::Error;
+use std::sync::atomic::Ordering;
+use std::time::SystemTime;
 
-use tauri::{App, GlobalShortcutManager, Manager};
+use rdev::{
+    Button::Left,
+    Event,
+    EventType::{ButtonPress, ButtonRelease},
+};
+use tauri::{App, Manager};
 
-use crate::{shortcut, util};
+use crate::{common::TIME, shortcut, util};
 
 pub fn handler(app: &mut App) -> Result<(), Box<dyn Error>> {
-    if cfg!(any(target_os = "windows", target_os = "macos")) {
-        util::decor(app, "panel");
-    };
     let panel = app.get_window("panel").expect("Failed to get panel window");
 
-    // 全局快捷键
-    // Global shortcut
-    app.global_shortcut_manager()
-        .register("Alt + X", move || {
-            shortcut::show(&panel).expect("Shortcut key call failed")
+    // 监听鼠标左键释放
+    // Listen mouse left button release
+    tokio::spawn(async {
+        rdev::listen(move |event: Event| match event.event_type {
+            ButtonPress(Left) => {
+                // 储存 按下时间戳
+                // Store press timestamp
+                TIME.store(
+                    event
+                        .time
+                        .duration_since(SystemTime::UNIX_EPOCH)
+                        .unwrap()
+                        .as_millis() as usize,
+                    Ordering::Relaxed,
+                )
+            }
+            ButtonRelease(Left) => {
+                // 识别选择状态
+                // Recognize selection status
+                let now = event
+                    .time
+                    .duration_since(SystemTime::UNIX_EPOCH)
+                    .unwrap()
+                    .as_millis() as usize;
+                let time = TIME.load(Ordering::Relaxed);
+                if now - time < 300 {
+                    return;
+                }
+                shortcut::show(&panel).expect("Shortcut key call failed");
+            }
+            _ => {}
         })
-        .expect("Failed to register global shortcut");
+    });
+
     Ok(())
 }
